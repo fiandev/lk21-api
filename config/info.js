e5a6b0e977efbfb901fad743cdd32d8fef6aac5b
fiandev <wongkeras2@gmail.com>
2022-05-27 19:34:04 +0700

    update

--- config/info.js
+++ config/info.js
@@ -2,4 +2,4 @@ const author = "ryuudev"
 const time = new Date()
 const date = `${time.getDate()}, ${time.getMonth() + 1} ${time.getFullYear()} ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
 
-export default {time, date}
+export {time, date}
