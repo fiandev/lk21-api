d743de9063f5cad058a271368405517aecffa0bd
fiandev <wongkeras2@gmail.com>
2022-05-27 15:46:59 +0700

    fix

--- utils/getDetailMovie.js
+++ utils/getDetailMovie.js
@@ -6,14 +6,6 @@ const getDetailMovie = ({ htmlCode }) => {
   const { window } = new JSDOM(htmlCode);
   const movieCards = window.document.querySelector("#movie-detail .content");
   let totalPages = 1;
-  if (window.document.querySelector("#pagination span") != null) {
-    totalPages = Number(
-      window.document.querySelector("#pagination span").textContent.split(" ")[3]
-    );
-  /* not found */
-  } else {
-    return false
-  }
   let result = [];
   let movie = {};
   const kualitas = movieCards[0].querySelector("h3").textContent
