d6cc2aa4f726ebdcebe8c136542b7ef2fb5d7e0c
fiandev <wongkeras2@gmail.com>
2022-05-27 16:02:29 +0700

    fix

--- utils/getDetailMovie.js
+++ utils/getDetailMovie.js
@@ -6,15 +6,12 @@ const getDetailMovie = ({ htmlCode }) => {
   const { window } = new JSDOM(htmlCode);
   const movieCards = window.document.querySelectorAll(".post .content .content-wrapper .row .content div");
   let totalPages = 1;
-  let result = [];
-  let movie = {};
+  let result = {};
   movieCards.forEach((detail) => {
     let key = detail.querySelector("h2").textContent
     let value = detail.querySelector("h3").textContent
-    movie[key] = value
+    result[key] = value
   });
-  result.push(movie);
-
   return { result, totalPages };
 };
 
