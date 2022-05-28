219fc322a65f30c332d09e6d1f22b8d4d1d20c37
fiandev <wongkeras2@gmail.com>
2022-05-28 09:30:46 +0700

    update

--- utils/getLinkDownload.js
+++ utils/getLinkDownload.js
@@ -5,9 +5,9 @@ const { JSDOM } = jsdom;
 
 const getLinkDownload = ({ htmlCode }) => {
   const { window } = new JSDOM(htmlCode);
-  let result = [];
-  const container = window.document.queryselector("#results")
+  const container = window.document.querySelector("#results")
   const tables = container.querySelectorAll(".table-responsive table")
+  let result = [];
   
   tables.forEach(table => {
     const jsonTable = new HtmlTableToJson(table);
